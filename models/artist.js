a9fa427c374d3091df97e102f49339463495af26
alicht <avilichtschein@gmail.com>
2017-10-26 23:03:43 -0400

    added info to artist.js module in models

--- models/artist.js
+++ models/artist.js
@@ -1,1 +1,11 @@
-artist.js
+module.exports = (sequelize, DataTypes) => {
+   return sequelize.define('artist', {
+     name: DataTypes.STRING,
+     birthday: DataTypes.DATEONLY,
+     deathday: DataTypes.DATEONLY,
+     gender: DataTypes.INTEGER,
+     placeOfBirth: DataTypes.STRING
+   }, {
+     timestamps: false
+   });
+ }
